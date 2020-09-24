28cdd67755b50fc0227c122d922c661bb1d8f3a4
Alex Olkhovskiy <lxol@users.noreply.github.com>
2020-09-24 08:59:37 +0100

    K3 done

--- build.sbt
+++ build.sbt
@@ -7,4 +7,31 @@ libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
 
 // scalac options come from the sbt-tpolecat plugin so need to set any here
 
-addCompilerPlugin("org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full)
+libraryDependencies += "com.lihaoyi" % "ammonite" % "2.2.0-4-4bd225e" cross CrossVersion.full
+
+// libraryDependencies += {
+//   val version = scalaBinaryVersion.value match {
+//     case "2.10" => "1.0.3"
+//     case _ ⇒ "2.2.0"
+//   }
+//   "com.lihaoyi" % "ammonite" % version % "test" cross CrossVersion.full
+// }
+
+// sourceGenerators in Test += Def.task {
+//   val file = (sourceManaged in Test).value / "amm.scala"
+//   IO.write(file, """object amm extends App { ammonite.Main.main(args) }""")
+//   Seq(file)
+// }.taskValue
+
+// // Optional, required for the `source` command to work
+// (fullClasspath in Test) ++= {
+//   (updateClassifiers in Test).value
+//     .configurations
+//     .find(_.configuration.name == Test.name)
+//     .get
+//     .modules
+//     .flatMap(_.artifacts)
+//     .collect{case (a, f) if a.classifier == Some("sources") => f}
+// }
+
+// ddCompilerPlugin("org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full)
--- src/main/scala/swc2/Chapter11.scala
+++ src/main/scala/swc2/Chapter11.scala
@@ -1,17 +1,44 @@
 package swc2
+import cats.kernel.CommutativeMonoid
+import cats.implicits._
+import cats.kernel.Monoid
 
-final case class GCounter(counters: Map[String, Int]) {
-  def increment(machine: String, amount: Int): GCounter = {
-    val value = amount + counters.getOrElse(machine, 0)
-    GCounter(counters + (machine -> value))
+trait BoundedSemiLattice[A] extends CommutativeMonoid[A] {
+  def combine(a1: A, a2: A): A
+  def empty: A
+}
+
+object BoundedSemiLattice {
+  implicit val intBoundedSemiLattice = new BoundedSemiLattice[Int] {
+    def combine(a1: Int, a2: Int): Int = a1 max a2
+    def empty: Int = 0
   }
 
-  def merge(that: GCounter): GCounter = {
-    GCounter(counters ++ that.counters.map {
-      case (k, v) => (k, v max that.counters.getOrElse(k, 0))
-    })
-
+  implicit def setsBoundedSemiLattice[A]() = new BoundedSemiLattice[Set[A]] {
+    def combine(s1: Set[A], s2: Set[A]): Set[A] = s1 combine s2
+    def empty: Set[A] = Set[A]()
   }
-  def total: Int = counters.values.sum
+}
 
+final case class GCounter[A](counters: Map[String, A]) {
+  def increment(machine: String, amount: A)(
+      implicit M: Monoid[A]
+  ): GCounter[A] =
+    GCounter(
+      counters + (machine -> M
+        .combine(amount, counters.getOrElse(machine, M.empty)))
+    )
+
+  def merge(
+      that: GCounter[A]
+  )(implicit B: BoundedSemiLattice[A]): GCounter[A] = {
+    GCounter(this.counters |+| that.counters)
+    // GCounter[A](counters ++ that.counters.map {
+    //   case (k, v) => (k, B.combine(v, that.counters.getOrElse(k, B.empty)))
+    // })
+  }
+
+  def totalm(implicit C: CommutativeMonoid[A]): A =
+    // counters.values.foldLeft(C.empty)((acc, e) => C.combine(acc, e))
+    counters.values.toList.combineAll
 }
