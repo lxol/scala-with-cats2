1eb7a085ae9160262446d275f897854ba8e3aa45
Alex Olkhovskiy <lxol@users.noreply.github.com>
2020-10-02 08:34:11 +0100

    completed

--- src/main/scala/swc2/Solution.scala
+++ src/main/scala/swc2/Solution.scala
@@ -42,17 +42,55 @@ object Solution {
   object GCounter {
     def apply[F[_, _], K, V](implicit counter: GCounter[F, K, V]) =
       counter
-
   }
 
-  implicit def mGCounter[K, V](): GCounter[Map, K, V] =
-    new GCounter[Map, K, V] {
-      override def increment(f: Map[K, V])(k: K, v: V)(implicit m: CommutativeMonoid[V]): Map[K, V] =
-        f |+| Map(k -> v)
+//  implicit def mGCounter[K, V](): GCounter[Map, K, V] =
+//    new GCounter[Map, K, V] {
+//      override def increment(f: Map[K, V])(k: K, v: V)(implicit m: CommutativeMonoid[V]): Map[K, V] =
+//        f |+| Map(k -> v)
+//
+//      override def merge(f1: Map[K, V], f2: Map[K, V])(implicit b: BoundedSemiLattice[V]): Map[K, V] =
+//        f1 |+| f2
+//
+//      override def total(f: Map[K, V])(implicit m: CommutativeMonoid[V]): V = f.values.toList.combineAll
+//    }
 
-      override def merge(f1: Map[K, V], f2: Map[K, V])(implicit b: BoundedSemiLattice[V]): Map[K, V] =
+  trait KeyValueStore[F[_, _]] {
+    def put[K, V](f: F[K, V])(k: K, v: V): F[K, V]
+    def get[K, V](f: F[K, V])(k: K): Option[V]
+    def getOrElse[K, V](f: F[K, V])(k: K, default: V): V =
+      get(f)(k).getOrElse(default)
+    def values[K, V](f: F[K, V]): List[V]
+  }
+
+  object KeyValueStore {
+    implicit val mapKeyValueStore: KeyValueStore[Map] =
+      new KeyValueStore[Map] {
+        override def put[K, V](f: Map[K, V])(k: K, v: V): Map[K, V] = f + (k -> v)
+        override def get[K, V](f: Map[K, V])(k: K): Option[V] = f.get(k)
+        override def values[K, V](f: Map[K, V]): List[V] = f.values.toList
+      }
+
+    implicit class KvsOps[F[_, _], K, V](f: F[K, V]) {
+      def put(key: K, value: V)(implicit kvs: KeyValueStore[F]): F[K, V] =
+        kvs.put(f)(key, value)
+      def get(key: K)(implicit kvs: KeyValueStore[F]): Option[V] =
+        kvs.get(f)(key)
+      def getOrElse(key: K, default: V)(implicit kvs: KeyValueStore[F]): V =
+        kvs.getOrElse(f)(key, default)
+      def values(implicit kvs: KeyValueStore[F]): List[V] = kvs.values(f)
+    }
+  }
+  import Solution.KeyValueStore._
+  implicit def gcounterInstance[F[_, _], K, V](implicit kvs: KeyValueStore[F], km: CommutativeMonoid[F[K, V]]) =
+    new GCounter[F, K, V] {
+      def increment(f: F[K, V])(key: K, value: V)(implicit m: CommutativeMonoid[V]): F[K, V] = {
+        val total = f.getOrElse(key, m.empty) |+| value
+        f.put(key, total)
+      }
+      def merge(f1: F[K, V], f2: F[K, V])(implicit b: BoundedSemiLattice[V]): F[K, V] =
         f1 |+| f2
-
-      override def total(f: Map[K, V])(implicit m: CommutativeMonoid[V]): V = f.values.toList.combineAll
+      def total(f: F[K, V])(implicit m: CommutativeMonoid[V]): V =
+        f.values.combineAll
     }
 }
